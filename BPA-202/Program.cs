ecd5afcec9718299161e86ff6dc41a2224f0d2cb
YousefV10 <yusifrv-bpa202@code.edu.az>
2025-10-07 11:01:55 +0400

    Add project files.

---
+++ BPA-202/Program.cs
@@ -0,0 +1,102 @@
+﻿// Fibanoccii)
+
+//int firNumber = 0;
+//int secNumber = 1;
+//Console.WriteLine(firNumber);
+//Console.WriteLine(secNumber);
+//int n = 8;
+//for(int i=2; i < n; i++)
+//{
+//    int thirdNumber = firNumber + secNumber;
+//    firNumber = secNumber;
+//    secNumber = thirdNumber;
+//    Console.WriteLine(thirdNumber);
+//}
+
+//--------------------------------------------------------------------------------------
+
+
+// Task 2 
+
+//int x = 20;
+//int y = 21;
+//int a = 0;
+
+//for (int i = 0; i <= x; i++)
+//{
+//    if (i % y == 0)
+//    {
+//        if (a < i)
+//        {
+//            a = i;
+
+//        }
+//    }
+
+
+
+//}
+//if (a!=0)
+//{
+//    Console.WriteLine(a);
+//}
+//else
+//{
+//    Console.WriteLine(-1);
+//}
+
+//--------------------------------------------------------------------------
+
+// Task 3 
+
+//int n = 15; 
+//for (int i=1; i<=n; i++)
+//{
+//    if (n % i == 0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+
+//--------------------------------------------------------------------------------
+
+
+// Task 4 
+
+
+//int a = 3475;
+//int b = 0;
+//int c=0;
+//while (a > 0)
+//{
+  
+//    b = a % 10;
+//    a = a / 10;
+//    if (c < b) {
+//        c = b;
+
+//    }
+    
+//}
+//Console.WriteLine(c);
+
+//int season = a % 4;
+//switch (season)
+//{
+//    case 1:
+//        Console.WriteLine("Winter");
+//        break;
+
+//    case 2:
+//        Console.WriteLine("Spring ");
+//        break;
+
+//    case 3:
+//        Console.WriteLine("Summer ");
+//        break;
+
+//    case 0:
+//        Console.WriteLine("Autumn");
+//        break;
+//}
